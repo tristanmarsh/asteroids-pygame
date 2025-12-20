944f11afb6cf6ec2f0b49b05e3007a42aa319983
Tristan Marsh <tristanmarsh@live.com>
2025-12-21 00:08:27 +1100

    target 60fps

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ def main():
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -18,6 +21,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
