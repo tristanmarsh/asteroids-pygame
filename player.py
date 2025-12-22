1d95d21122c4eb9d24ddfef0c7fddda9c9d3e9e8
Tristan Marsh <tristanmarsh@live.com>
2025-12-22 11:57:00 +1100

    move player

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 
@@ -30,3 +30,15 @@ class Player(CircleShape):
 
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
+
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(-dt)
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
