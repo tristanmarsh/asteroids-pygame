d60b1332c5d0d67825e13a0e82ae379024ffd5c6
Tristan Marsh <tristanmarsh@live.com>
2025-12-30 00:12:39 +1100

    add support for vim controls

--- player.py
+++ player.py
@@ -33,16 +33,16 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT] or keys[pygame.K_h]:
             self.rotate(-dt)
 
-        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT] or keys[pygame.K_l]:
             self.rotate(dt)
 
-        if keys[pygame.K_w] or keys[pygame.K_UP]:
+        if keys[pygame.K_w] or keys[pygame.K_UP] or keys[pygame.K_k]:
             self.move(dt)
 
-        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        if keys[pygame.K_s] or keys[pygame.K_DOWN] or keys[pygame.K_j]:
             self.move(-dt)
 
         if keys[pygame.K_SPACE]:
